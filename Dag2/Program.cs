95e5d6f73db42e6689fad2f9151a67143bb79179
SimonEckerud <simoneckerud@gmail.com>
2021-01-19 10:20:37 +0100

    Initial commit

---
+++ Dag2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dag2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
